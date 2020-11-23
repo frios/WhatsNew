0bc0fcb71e92eca8628c21dbdb2294d697e5acee
Ferdinand Rios <frios@viciouscircle.com>
2020-11-23 13:44:21 -0800

    more

--- Sources/WhatsNew/WhatsNewView.swift
+++ Sources/WhatsNew/WhatsNewView.swift
@@ -12,8 +12,8 @@ public struct WhatsNewView<Content: View>: View {
     
     let content: [Content]
     
-    public init(@ViewBuilder content: () -> [Content]){
-        self.content = content()
+    public init(content: [Content]){
+        self.content = content
     }
         
     public var body: some View {
