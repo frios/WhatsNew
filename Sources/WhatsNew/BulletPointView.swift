33582f2b382a4d631815842c8212ea3ab505ab48
Ferdinand Rios <frios@viciouscircle.com>
2021-12-17 09:01:59 -0800

    image width = 60

--- Sources/WhatsNew/BulletPointView.swift
+++ Sources/WhatsNew/BulletPointView.swift
@@ -26,7 +26,7 @@ public struct BulletPointView: View {
         HStack (alignment: .center){
             image
 //                .resizable()
-                .frame(width:50)
+                .frame(width:60)
                 .scaledToFit()
                 .foregroundColor(Color("AccentColor"))
             VStack (alignment: .leading, spacing: 4){
