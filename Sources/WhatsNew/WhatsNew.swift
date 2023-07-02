10d441e1242e4db33f7c080fe5de9ff4d240b177
Stephane Peter <megastep@megastep.org>
2023-07-01 20:19:20 -0700

    Better version comparison logic.

--- Sources/WhatsNew/WhatsNew.swift
+++ Sources/WhatsNew/WhatsNew.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 public class WhatsNew: ObservableObject {
     
-    @AppStorage("savedVersion") var savedVersion: String = "1.000.000"
+    @AppStorage("savedVersion") var savedVersion: String?
     
     var alwaysShow = false
-
+    
     public init(alwaysShow: Bool = false) {
         self.alwaysShow = alwaysShow
     }
@@ -21,26 +21,32 @@ public class WhatsNew: ObservableObject {
     //check the currentVersion against the saved version
     public func checkForUpdate( showWhatsNew: Binding<Bool> ) {
         let currentVersion = getCurrentAppVersion()
-        #if DEBUG
-        print("Current Version: \(currentVersion)\nSaved Version: \(savedVersion)")
-        #endif
+#if DEBUG
+        print("Current Version: \(currentVersion)\nSaved Version: \(savedVersion ?? "None")")
+#endif
         if alwaysShow {
             showWhatsNew.wrappedValue = alwaysShow
             return
         }
         
-        if savedVersion < currentVersion {
-            savedVersion = currentVersion
+        // If this is the first launch (no saved version), save the current version and return
+        guard let savedVersion = savedVersion else {
+            self.savedVersion = currentVersion
+            return
+        }
+        
+        if isVersion(savedVersion, lessThan: currentVersion) {
+            self.savedVersion = currentVersion
             showWhatsNew.wrappedValue = true
-        } else if savedVersion > currentVersion {
-            #if DEBUG
+        } else if isVersion(savedVersion, greaterThan: currentVersion) {
+#if DEBUG
             print("App is behind!")
-            #endif
-            savedVersion = currentVersion
+#endif
+            self.savedVersion = currentVersion
         } else {
-            #if DEBUG
+#if DEBUG
             print("App is up to date!")
-            #endif
+#endif
         }
     }
     
@@ -48,6 +54,27 @@ public class WhatsNew: ObservableObject {
     func getCurrentAppVersion() -> String {
         return "\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String).\(Bundle.main.infoDictionary?["CFBundleVersion"] as! String)"
     }
+    
+    // Compare version strings as version numbers
+    private func isVersion(_ version1: String, lessThan version2: String) -> Bool {
+        let v1Components = version1.split(separator: ".").compactMap { Int($0) }
+        let v2Components = version2.split(separator: ".").compactMap { Int($0) }
+        
+        for (v1, v2) in zip(v1Components, v2Components) {
+            if v1 < v2 {
+                return true
+            } else if v1 > v2 {
+                return false
+            }
+        }
+        
+        return v1Components.count < v2Components.count
+    }
+    
+    private func isVersion(_ version1: String, greaterThan version2: String) -> Bool {
+        return !isVersion(version1, lessThan: version2) && version1 != version2
+    }
+    
 }
 
 
